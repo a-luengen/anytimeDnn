a11867604d2d33b32c2a20db08cf2918be20bb5a
a.luengen <alex.luengen@melodyscanner.com>
2020-07-15 11:01:27 +0200

    Refactored dataloader for reuse.

--- msdnet/dataloader.py
+++ msdnet/dataloader.py
@@ -6,41 +6,53 @@ import os
 
 
 def get_dataloaders(args):
+    return get_dataloaders_alt(
+        args.data_root, 
+        args.data, 
+        args.use_valid, 
+        args.save, 
+        args.batch_size, 
+        args.workers,
+        args.splits
+        )
+
+
+def get_dataloaders_alt(data_root, data, use_valid, save, batch_size, workers, splits):
     train_loader, val_loader, test_loader = None, None, None
-    if args.data == 'cifar10':
+    if data == 'cifar10':
         normalize = transforms.Normalize(mean=[0.4914, 0.4824, 0.4467],
                                          std=[0.2471, 0.2435, 0.2616])
-        train_set = datasets.CIFAR10(args.data_root, train=True,
+        train_set = datasets.CIFAR10(data_root, train=True,
                                      transform=transforms.Compose([
                                         transforms.RandomCrop(32, padding=4),
                                         transforms.RandomHorizontalFlip(),
                                         transforms.ToTensor(),
                                         normalize
                                      ]))
-        val_set = datasets.CIFAR10(args.data_root, train=False,
+        val_set = datasets.CIFAR10(data_root, train=False,
                                    transform=transforms.Compose([
                                     transforms.ToTensor(),
                                     normalize
                                    ]))
-    elif args.data == 'cifar100':
+    elif data == 'cifar100':
         normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408],
                                          std=[0.2675, 0.2565, 0.2761])
-        train_set = datasets.CIFAR100(args.data_root, train=True,
+        train_set = datasets.CIFAR100(data_root, train=True,
                                       transform=transforms.Compose([
                                         transforms.RandomCrop(32, padding=4),
                                         transforms.RandomHorizontalFlip(),
                                         transforms.ToTensor(),
                                         normalize
                                       ]))
-        val_set = datasets.CIFAR100(args.data_root, train=False,
+        val_set = datasets.CIFAR100(data_root, train=False,
                                     transform=transforms.Compose([
                                         transforms.ToTensor(),
                                         normalize
                                     ]))
     else:
         # ImageNet
-        traindir = os.path.join(args.data_root, 'train')
-        valdir = os.path.join(args.data_root, 'val')
+        traindir = os.path.join(data_root, 'train')
+        valdir = os.path.join(data_root, 'val')
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
         train_set = datasets.ImageFolder(traindir, transforms.Compose([
@@ -55,47 +67,47 @@ def get_dataloaders(args):
             transforms.ToTensor(),
             normalize
         ]))
-    if args.use_valid:
+    if use_valid:
         train_set_index = torch.randperm(len(train_set))
-        if os.path.exists(os.path.join(args.save, 'index.pth')):
+        if os.path.exists(os.path.join(save, 'index.pth')):
             print('!!!!!! Load train_set_index !!!!!!')
-            train_set_index = torch.load(os.path.join(args.save, 'index.pth'))
+            train_set_index = torch.load(os.path.join(save, 'index.pth'))
         else:
             print('!!!!!! Save train_set_index !!!!!!')
-            torch.save(train_set_index, os.path.join(args.save, 'index.pth'))
-        if args.data.startswith('cifar'):
+            torch.save(train_set_index, os.path.join(save, 'index.pth'))
+        if data.startswith('cifar'):
             num_sample_valid = 5000
         else:
             num_sample_valid = 50000
 
-        if 'train' in args.splits:
+        if 'train' in splits:
             train_loader = torch.utils.data.DataLoader(
-                train_set, batch_size=args.batch_size,
+                train_set, batch_size=batch_size,
                 sampler=torch.utils.data.sampler.SubsetRandomSampler(
                     train_set_index[:-num_sample_valid]),
-                num_workers=args.workers, pin_memory=True)
-        if 'val' in args.splits:
+                num_workers=workers, pin_memory=True)
+        if 'val' in splits:
             val_loader = torch.utils.data.DataLoader(
-                train_set, batch_size=args.batch_size,
+                train_set, batch_size=batch_size,
                 sampler=torch.utils.data.sampler.SubsetRandomSampler(
                     train_set_index[-num_sample_valid:]),
-                num_workers=args.workers, pin_memory=True)
-        if 'test' in args.splits:
+                num_workers=workers, pin_memory=True)
+        if 'test' in splits:
             test_loader = torch.utils.data.DataLoader(
                 val_set,
-                batch_size=args.batch_size, shuffle=False,
-                num_workers=args.workers, pin_memory=True)
+                batch_size=batch_size, shuffle=False,
+                num_workers=workers, pin_memory=True)
     else:
-        if 'train' in args.splits:
+        if 'train' in splits:
             train_loader = torch.utils.data.DataLoader(
                 train_set,
-                batch_size=args.batch_size, shuffle=True,
-                num_workers=args.workers, pin_memory=True)
-        if 'val' or 'test' in args.splits:
+                batch_size=batch_size, shuffle=True,
+                num_workers=workers, pin_memory=True)
+        if 'val' or 'test' in splits:
             val_loader = torch.utils.data.DataLoader(
                 val_set,
-                batch_size=args.batch_size, shuffle=False,
-                num_workers=args.workers, pin_memory=True)
+                batch_size=batch_size, shuffle=False,
+                num_workers=workers, pin_memory=True)
             test_loader = val_loader
 
     return train_loader, val_loader, test_loader
