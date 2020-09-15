092b13a9a30ef2f6df14d76d5a11a361dc70617c
a.luengen <alex.luengen@melodyscanner.com>
2020-09-15 16:00:25 +0200

    Added min/max logging to AverageMeter. Added support for DataParallel state_dict when resuming from checkpoint.

--- utils.py
+++ utils.py
@@ -27,27 +27,6 @@ def get_msd_net_model():
     obj.reduction = 0.5 # compression of densenet
     return MSDNet(obj)
 
-def save_checkpoint(state, is_best: bool, arch: str, checkpoint_dir: str, filename=None):
-    if filename is None:
-        filename = state["arch"] + '_checkpoint.pth.tar'
-
-    if not os.path.isdir(checkpoint_dir):
-        os.mkdir(checkpoint_dir)
-    
-    torch.save(state, filename)
-    logging.debug(os.path.join(checkpoint_dir, filename))
-
-    target = os.path.join(os.path.basename(checkpoint_dir), filename)
-    if os.path.exists(target):
-        os.remove(target)
-    shutil.move(filename, os.path.basename(checkpoint_dir))
-
-    if is_best:
-        best_filename = state["arch"] + '_model_best.pth.tar'
-        best_filePath = os.path.join(checkpoint_dir, best_filename)
-        source = os.path.join(checkpoint_dir, filename)
-        shutil.copyfile(source, best_filePath)
-
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
@@ -58,15 +37,19 @@ class AverageMeter(object):
         self.val = 0
         self.name = name
         self.fmt = fmt
+        self.max = 0
+        self.min = float('inf')
 
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+        if self.max < val: self.max = val
+        if self.min > val: self.min = val
 
     def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '}) ({min' + self.fmt + '}) ({max' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
 
 def get_batch_size_stats(loader):
@@ -100,6 +83,8 @@ def getModel(arch: str):
     model = None
     if arch == 'resnet50':
         model = ResNet.resnet50()
+    if arch == 'resnet50-pol':
+        model = ResNet.resnet50(use_policy=True)
     elif arch == 'resnet101':
         model = ResNet.resnet101()
     elif arch == 'resnet152':
@@ -108,6 +93,8 @@ def getModel(arch: str):
         model = dn.DenseNet3(3, 40)
     elif arch == 'densenet121':
         model = dn.DenseNet4([6, 12, 24, 16], 40, growth_rate=32)
+    elif arch == 'densenet169':
+        model = dn.DenseNet4([6, 12, 32, 32], 40, growth_rate=32)
     elif arch == 'msdnet':
         model = get_msd_net_model()
     else:
@@ -116,9 +103,64 @@ def getModel(arch: str):
 
 def getStateDict(model, epoch : int, arch : str, best_acc: float, optimizer):
     return {
-        'epoch': epoch + 1,
+        'epoch': epoch,
         'arch' : arch,
         'state_dict': model.state_dict(),
         'best_acc': best_acc,
         'optimizer': optimizer.state_dict(),
     }
+
+CHECKPOINT_POSTFIX = '_checkpoint.pth.tar'
+CHECKPOINT_BEST_POSTFIX = '_model_best.pth.tar'
+
+def save_checkpoint(state, is_best: bool, arch: str, checkpoint_dir: str, filename=None):
+    if filename is None:
+        filename = state["arch"] + CHECKPOINT_POSTFIX
+
+    if not os.path.isdir(checkpoint_dir):
+        os.mkdir(checkpoint_dir)
+    
+    torch.save(state, filename)
+    logging.debug(os.path.join(checkpoint_dir, filename))
+
+    target = os.path.join(os.path.basename(checkpoint_dir), filename)
+    if os.path.exists(target):
+        os.remove(target)
+    shutil.move(filename, os.path.basename(checkpoint_dir))
+
+    if is_best:
+        best_filename = state["arch"] + CHECKPOINT_BEST_POSTFIX
+        best_filePath = os.path.join(checkpoint_dir, best_filename)
+        source = os.path.join(checkpoint_dir, filename)
+        shutil.copyfile(source, best_filePath)
+
+def resumeFromPath(path : str, model):
+    start_epoch = 0
+    best_prec1 = 0.0
+
+    if not os.path.isfile(path):
+        print(f'No file found {path}')
+        logging.info(f"=> no checkpoint found at '{path}'")
+        return model, start_epoch, best_prec1
+
+    logging.debug(f"=> loading checkpoint {path}")
+    
+    if not (torch.cuda.is_available() and isinstance(model, torch.nn.DataParallel)):
+        checkpoint = torch.load(path, map_location=torch.device('cpu'))
+        # remove 'module.' string before keys in state_dict
+        from collections import OrderedDict
+        new_state_dict = OrderedDict()
+        for k,v in checkpoint['state_dict'].items():
+            name = k.replace('module.', '') # remove 'module.'
+            new_state_dict[name] = v
+        checkpoint['state_dict'] = new_state_dict
+    else:
+        checkpoint = torch.load(path)
+    
+    model.load_state_dict(checkpoint['state_dict'])
+    
+    start_epoch = checkpoint['epoch']
+    best_prec1 = checkpoint['best_acc']
+    logging.info(f"=> loaded checkpoint '{path}' (epoch {checkpoint['epoch']})")
+
+    return model, start_epoch, best_prec1
