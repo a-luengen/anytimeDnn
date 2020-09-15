e7bca015e463df42324a75a17f14439626aa8b73
a.luengen <alex.luengen@melodyscanner.com>
2020-09-15 16:13:51 +0200

    Fixed dataloader to centercrop 224 instead of 225 pixels.

--- msdnet/dataloader.py
+++ msdnet/dataloader.py
@@ -52,19 +52,20 @@ def get_dataloaders_alt(data_root, data, use_valid, save, batch_size, workers, s
                                     ]))
     else:
         # ImageNet
+        logging.info(f"Creating train_set and val_set loaders: Batch = {batch_size}, Worker = {workers}, Splits = {splits} ")
         traindir = os.path.join(data_root, 'train')
         valdir = os.path.join(data_root, 'val')
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
         train_set = datasets.ImageFolder(traindir, transforms.Compose([
-            transforms.RandomResizedCrop(225),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize
         ]))
         val_set = datasets.ImageFolder(valdir, transforms.Compose([
             transforms.Resize(256),
-            transforms.CenterCrop(225),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize
         ]))
