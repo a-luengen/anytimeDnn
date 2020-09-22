f777a99682108a26c0e8d11d7be05a0e7981afa6
a.luengen <alex.luengen@melodyscanner.com>
2020-09-22 10:58:23 +0200

    Minor modernization to msdnet dataloader.

--- msdnet/dataloader.py
+++ msdnet/dataloader.py
@@ -17,6 +17,76 @@ def get_dataloaders(args):
         args.splits
         )
 
+def get_imagenet_dataloaders(data_root, use_valid, save, batch_size, workers, splits):
+    train_loader, val_loader, test_loader = None, None, None
+
+    # ImageNet
+    logging.info(f"Creating train_set and val_set loaders: Batch = {batch_size}, Worker = {workers}, Splits = {splits} ")
+    traindir = os.path.join(data_root, 'train')
+    valdir = os.path.join(data_root, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225])
+    train_set = datasets.ImageFolder(traindir, transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ]))
+    val_set = datasets.ImageFolder(valdir, transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize
+    ]))
+
+    if use_valid:
+        train_set_index = torch.randperm(len(train_set))
+        if os.path.exists(os.path.join(save, 'index.pth')):
+            logging.info('!!!!!! Load train_set_index !!!!!!')
+            train_set_index = torch.load(os.path.join(save, 'index.pth'))
+        else:
+            logging.info('!!!!!! Save train_set_index !!!!!!')
+            os.mkdir(save)
+            torch.save(train_set_index, os.path.join(save, 'index.pth'))
+
+        if data.startswith('cifar'):
+            num_sample_valid = 5000
+        else:
+            #num_sample_valid = 50000
+            # take 10% of the train set as validation set
+            num_sample_valid = int(len(train_set) * 0.1)
+
+        if 'train' in splits:
+            train_loader = torch.utils.data.DataLoader(
+                train_set, batch_size=batch_size,
+                sampler=torch.utils.data.sampler.SubsetRandomSampler(
+                    train_set_index[:-num_sample_valid]),
+                num_workers=workers, pin_memory=True)
+        if 'val' in splits:
+            val_loader = torch.utils.data.DataLoader(
+                train_set, batch_size=batch_size,
+                sampler=torch.utils.data.sampler.SubsetRandomSampler(
+                    train_set_index[-num_sample_valid:]),
+                num_workers=workers, pin_memory=True)
+        if 'test' in splits:
+            test_loader = torch.utils.data.DataLoader(
+                val_set,
+                batch_size=batch_size, shuffle=False,
+                num_workers=workers, pin_memory=True)
+    else:
+        if 'train' in splits:
+            train_loader = torch.utils.data.DataLoader(
+                train_set,
+                batch_size=batch_size, shuffle=True,
+                num_workers=workers, pin_memory=True)
+        if 'val' or 'test' in splits:
+            val_loader = torch.utils.data.DataLoader(
+                val_set,
+                batch_size=batch_size, shuffle=False,
+                num_workers=workers, pin_memory=True)
+            test_loader = val_loader
+
+    return train_loader, val_loader, test_loader
 
 def get_dataloaders_alt(data_root, data, use_valid, save, batch_size, workers, splits):
     train_loader, val_loader, test_loader = None, None, None
