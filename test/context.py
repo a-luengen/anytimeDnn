8fafb2913a0a83c03c0c8a65752061ea7ce915e4
a-luengen <a-luengen@web.de>
2020-10-11 13:38:48 +0200

    Added Torch and data_loader to Unittest-Context.

--- test/context.py
+++ test/context.py
@@ -1,10 +1,11 @@
 import os
 import sys
-
+import torch
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import utils
 import resnet.ResNet
 import resnet.utils as resnet_utils
 import data.utils as data_utils
+import data.ImagenetDataset as data_loader
 import densenet
