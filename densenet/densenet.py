47bfb11fa3cbecf4b62cb83a16831260d7ad0f1b
a-luengen <a-luengen@web.de>
2020-10-11 13:38:25 +0200

    Added spacing.

--- densenet/densenet.py
+++ densenet/densenet.py
@@ -32,6 +32,7 @@ class BottleneckBlock(nn.Module):
         self.droprate = dropRate
     def forward(self, x):
         out = self.conv1(self.relu(self.bn1(x)))
+        
         if self.droprate > 0:
             out = F.dropout(out, p=self.droprate, inplace=False, training=self.training)
         out = self.conv2(self.relu(self.bn2(out)))
@@ -58,11 +59,13 @@ class DenseBlock(nn.Module):
     def __init__(self, nb_layers, in_planes, growth_rate, block, dropRate=0.0):
         super(DenseBlock, self).__init__()
         self.layer = self._make_layer(block, in_planes, growth_rate, nb_layers, dropRate)
+
     def _make_layer(self, block, in_planes, growth_rate, nb_layers, dropRate):
         layers = []
         for i in range(nb_layers):
             layers.append(block(in_planes+i*growth_rate, growth_rate, dropRate))
         return nn.Sequential(*layers)
+
     def forward(self, x):
         return self.layer(x)
 
