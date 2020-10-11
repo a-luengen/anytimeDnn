e11627c15f02199405033306a85b4d027bccd9a8
a-luengen <a-luengen@web.de>
2020-10-11 13:39:13 +0200

    Improved type hinting

--- resnet/utils.py
+++ resnet/utils.py
@@ -2,6 +2,6 @@ import random
 import numpy as np
 from typing import List
 
-def getRandomBoolListPermutation(size: int, max_true: int) -> List:
+def getRandomBoolListPermutation(size: int, max_true: int) -> List[bool]:
     temp_list = [True] * (max_true) + [False] * (size - max_true)
     return np.random.permutation(temp_list)
