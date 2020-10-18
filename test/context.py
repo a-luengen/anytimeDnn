6c8ef76eb07b7209da5dd27f26e90b35310ce26a
a-luengen <a-luengen@web.de>
2020-10-18 16:41:06 +0200

    Updated unittest-context.

--- test/context.py
+++ test/context.py
@@ -9,3 +9,5 @@ import resnet.utils as resnet_utils
 import data.utils as data_utils
 import data.ImagenetDataset as data_loader
 import densenet
+import msdnet
+from train import accuracy as train_acc
