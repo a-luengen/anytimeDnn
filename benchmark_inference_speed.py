70d6351156409adff3d238c4ba93f242b4360f6a
a-luengen <a-luengen@web.de>
2020-10-26 16:21:44 +0100

    Refactored benchmark_inference_speed script.

--- benchmark_inference_speed.py
+++ benchmark_inference_speed.py
@@ -10,7 +10,6 @@ import logging
 import cProfile
 from tqdm import tqdm
 from timeit import default_timer as timer
-from torch.utils.data import DataLoader
 from sklearn import metrics
 
 import densenet.densenet as dn
@@ -23,13 +22,17 @@ from data.utils import getLabelToClassMapping
 #AVAILABLE_STATE_DICTS = ['densenet121', 'densenet121-skip']# 'densenet169']
 #AVAILABLE_STATE_DICTS = ['densenet121-skip']# 'densenet169']
 AVAILABLE_STATE_DICTS = ['resnet18-drop-rand-n', 'resnet34-drop-rand-n', 'resnet50-drop-rand-n']
+AVAILABLE_STATE_DICTS = ['resnet50-drop-rand-n']
+AVAILABLE_STATE_DICTS = ['resnet34-drop-rand-n']
+AVAILABLE_STATE_DICTS = ['densenet121']
 #AVAILABLE_STATE_DICTS = ['resnet18', 'resnet34', 'resnet50',]
 #AVAILABLE_STATE_DICTS = ['resnet18', 'resnet34', 'resnet50', 'densenet121', 'densenet169']
 STATE_DICT_PATH = os.path.join(os.getcwd(), 'state')
 BATCH_SIZE = 1
 SPEED_RUNS = 30
 QUALITY_RUNS = 10
-LAYERS_TO_SKIP = 1
+LAYERS_TO_SKIP = 0
+DATA_PATH = 'data/imagenet_red'
 
 def executeSpeedBench():
     # run benchmark loop
@@ -79,8 +82,8 @@ def executeQualityEvaluation(only_best):
         runQualityBenchForModel(arch, only_best=only_best)
 
 def runQualityBenchForModel(arch: str, only_best=False, persist_results=False)-> None:
-    data_path = 'data/imagenet_full'
-    #data_path = 'data/imagenet_red'
+    #data_path = 'data/imagenet_full'
+    data_path = DATA_PATH
 
     arch_states = [d for d in os.listdir(STATE_DICT_PATH) if arch.split("-")[0] in d]
     if only_best:
@@ -92,31 +95,31 @@ def runQualityBenchForModel(arch: str, only_best=False, persist_results=False)->
     with torch.no_grad():
         for state in arch_states:
             measurements = []
-            for i in range(0, QUALITY_RUNS):
-                model = getModelWithOptimized(arch, n=LAYERS_TO_SKIP)
+            for _ in range(0, QUALITY_RUNS):
+                model = getModelWithOptimized(arch, n=LAYERS_TO_SKIP, batch_size=BATCH_SIZE)
                 model.eval()
-                model, _, epoch, prec = resumeFromPath(os.path.join(STATE_DICT_PATH, state), model)
+                model, _, _, _ = resumeFromPath(os.path.join(STATE_DICT_PATH, state), model)
                 #logging.info(f"Resuming {state} from epoch {epoch} with best precision {prec}...")
                 classes = getLabelToClassMapping(os.path.join(os.getcwd(), data_path))
 
-                grndT, pred = evaluateModel(model, val_loader, classes)
+                grndT, pred = evaluateModel(model, val_loader, classes, BATCH_SIZE)
                 measurements.append(metrics.accuracy_score(grndT, pred))
                 if persist_results:
                     logging.info(metrics.classification_report(grndT, pred, digits=3))
                     generateAndStoreClassificationReportCSV(grndT, pred, f'{arch}_report.csv')
-
+                logging.debug(measurements)
             logging.info('')
             logging.info(measurements)
             logging.info(f'Precission: {sum(measurements) / len(measurements):.4f}')    
 
-def evaluateModel(model, loader, classes): 
+def evaluateModel(model, loader, classes, batch_size): 
     pred, grndT = [], []
     with tqdm(total=len(loader), desc="Progress", ncols=100) as pbar:
         for (images, labels) in iter(loader):
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
-            pred = pred + [classes[predicted[k]] for k in range(min(BATCH_SIZE, labels.shape[0]))]
-            grndT = grndT + [classes[labels[j]] for j in range(min(BATCH_SIZE, labels.shape[0]))]
+            pred = pred + [classes[predicted[k]] for k in range(min(batch_size, labels.shape[0]))]
+            grndT = grndT + [classes[labels[j]] for j in range(min(batch_size, labels.shape[0]))]
             pbar.update(1)
     return grndT, pred
 
