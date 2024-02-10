c3ed269eaa3a65a37a1dd0811843091be10c5546
a-luengen <a-luengen@web.de>
2024-02-10 15:50:27 +0100

    added missing updated

--- scripts/transferFiles.sh
+++ scripts/transferFiles.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-scp -r luengen@i82sn05.itec.kit.edu:~/Dokumente/anytimeDNN2/checkpoints ./../checkpoints/
+#scp -r luengen@i82sn05.itec.kit.edu:~/Dokumente/anytimeDNN2/checkpoints ./../checkpoints/
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet18_76__model_best.pth.tar ./../checkpoints/
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/densenet121_65__model_best.pth.tar ./../checkpoints/densenet121_model_best.pth.tar
+
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet18_76__model_best.pth.tar ./../checkpoints/resnet18_model_best.pth.tar
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet34_87__model_best.pth.tar ./../checkpoints/resnet34_model_best.pth.tar
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet50_86__model_best.pth.tar ./../checkpoints/resnet50_model_best.pth.tar
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/densenet169_64__model_best.pth.tar ./../checkpoints/densenet169__model_best.pth.tar
+
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet101_78__model_best.pth.tar ./../checkpoints/resnet101_model_best.pth.tar
+#scp ./../checkpoints/checkpoints.tar.gz luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/checkpoints.tar 
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet101_model_best.pth.tar ./../checkpoints/resnet101_model_best.pth.tar
+
+#scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpoints/resnet101_checkpoint.pth.tar ./../checkpoints/resnet101_checkpoint.pth.tar
+
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsResnet101/resnet101_85_model_best.pth.tar ./../state/resnet101_model_best.pth.tar
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsResnet101/resnet101_88_checkpoint.pth.tar ./../state/resnet101_checkpoint.pth.tar
+
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsMsdNet10/msdnet10_89_model_best.pth.tar ./../state/msdnet10_model_best.pth.tar.pth.tar
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsMsdNet10/msdnet10_89_checkpoint.pth.tar ./../state/msdnet10_checkpoint.pth.tar
+
+sshpass -f ".pw" scp ./../state/resnet152_model_best.pth.tar luengen@i82sn05.itec.kit.edu:~/Dokumente/anytimeDnn/state/resnet152_model_best.pth.tar
+
+sshpass -f ".pw" scp ./../state/resnet101_model_best.pth.tar luengen@i82sn05.itec.kit.edu:~/Dokumente/anytimeDnn/state/resnet101_model_best.pth.tar
+
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsNEU/resnet152_87_model_best.pth.tar ./../state/resnet152_model_best.pth.tar
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsNEU/resnet152_88_checkpoint.pth.tar ./../state/resnet152_checkpoint.pth.tar
+
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsNEU/resnet152_87_model_best.pth.tar ./../state/resnet152_model_best.pth.tar
+#sshpass -f ".pw" scp luengen@i82sn05.itec.kit.edu:~/anytimeDnnForHLR/checkpointsNEU/resnet152_88_checkpoint.pth.tar ./../state/resnet152_checkpoint.pth.tar
